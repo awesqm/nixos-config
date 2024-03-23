d4d865dde0f3407c57fb76b978915eb6a8433f92
Anton Yakovlev <sei@disroot.org>
2024-03-23 17:07:43 +0300

    add comments to unused files

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+# THIS FILE IS NO LONGER IN USE!
 
 { config, lib, pkgs, ... }:
 
